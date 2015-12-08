5bbb0ad87eaf49097c385e67cc5a020bb3cfe149
myjapan <myjapan@users.noreply.github.com>
2015-12-08 16:48:13 +0100

    Point toggling works as expected

--- stationmaster.ino
+++ stationmaster.ino
@@ -4,123 +4,69 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-int point_id[] = {0,1,2,3,4,5,6};
-int button_id[] = {2,3,4,5,6,7,8};
-int p_direction[] = {150,150,150,150,150,150,150};
-int p_straight[] = {150,150,150,150,150,150,150};
-int p_dev[] = {270,270,270,270,270,270,270};
-int l_straight[] = {7,8,9,10,11,12,13};
+byte button_total = 7;
+int button_id[] = {2,3,4,5,6,7,8};                  // toggle buttons
+int point_id[] = {0,1,3,4,5,7,8};                   // points connected to Adafruit 16-channel PWM servo driver
+int p_direction[] = {150,150,150,150,150,150,150};  // actual position
+int p_straight[] = {150,150,150,150,150,150,150};   // value to move the point in straight position
+int p_dev[] = {270,270,270,270,270,270,270};        // value to move the point in deviated position
+int l_straight[] = {7,8,9,10,11,12,13};             // leds (TBD)
 
-int button_total = 7;
+int reading;            // current reading from the input pin
+int previous = HIGH;    // previous reading from the input pin
 
-
-uint8_t inPin = 2;          // the number of the input pin. Il bottone è collegato tra il pin di ingresso e la massa
-uint8_t direzioneP0=150;
-int MINP0 = 150;
-int MAXP0 = 270;        // the current state of the output pin
-int reading;            // the current reading from the input pin
-int previous = HIGH;    // the previous reading from the input pin
-
-                        // the follow variables are long's because the time, measured in miliseconds,
-                        // will quickly become a bigger number than can be stored in an int.
-long time = 0;          // the last time the output pin was toggled
-byte debounce = 200;    // the debounce time, increase if the output flickers
+//long time = 0;
+byte debounce = 200;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 void setup()
 {
-  Serial.begin(4800);
+  Serial.begin(9600);
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-  
-  pinMode(inPin, INPUT_PULLUP);
-  pwm.setPWM(0, 0, direzioneP0);
 
-
- }
+  for(byte i = 0; i < button_total; i++)
+  {
+    pinMode(l_straight[i], OUTPUT);
+    pinMode(button_id[i], INPUT);
+    digitalWrite(button_id[i],HIGH);
+    pwm.setPWM(point_id[i], 0, p_straight[i]);
+    delay(300);
+  }
+}
 
 void loop()
 {
-
-   for(int index = 0; index < button_total; index++)  
-  {  
-    reading = digitalRead(button_id[index]);  // legge il valore di input  
-    if (reading == HIGH && previous == LOW && millis() - time > debounce)                              // controlla se il pulsante è premuto  
-    {  
-      if (index == 0)  
-      {  
-        time = millis();
-        toggle_point(point_id[index]);
-//        blink1(pinLed[index]);  
-      }  
-      if (index == 1)  
-      {  
-        time = millis();
-//        blink2(pinLed[indice]);  
-      }  
-      if (index == 2)  
-      {  
-        time = millis();
-//        blink3(pinLed[indice]);  
-      }  
-      if (index == 3)  
-      {  
-        time = millis();
-//        blink4(pinLed[indice]);  
-      }  
+   for(byte index = 0; index < button_total; index++)
+  {
+    reading = digitalRead(button_id[index]);  // legge il valore di input
+    if (reading == LOW && previous == HIGH) //  && millis() - time > debounce)      // check if a button was pressed
+    {
+          toggle_point(index);
     }
-    previous = reading;  
-//    else  
-//    {  
-//      digitalWrite(l_straight[index], LOW);          // spegne il LED  
-//    }  
+    previous = reading;
   }
+}
 
-//  reading = digitalRead(inPin);
-
-                        // if the input just went from LOW and HIGH and we've waited long enough
-                        // to ignore any noise on the circuit, toggle the output pin and remember
-                        // the time
-//  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-    
-}    
-
-void toggle_point(int which)
-{    
+void toggle_point(byte which)
+{
     if (p_direction[which] == p_straight[which])
       {
-      p_direction[which] = p_dev[which] ;
-      //pwm.setPWM(0, 0, direzioneP0);
+      p_direction[which] = p_dev[which];
       for (uint16_t pulselen = p_straight[which]; pulselen < p_dev[which]; pulselen++) {
-      pwm.setPWM(point_id[which], 0, pulselen);
-      delay(6);
+        pwm.setPWM(point_id[which], 0, pulselen);
+        delay(5);
       }
-      delay(500);
-      pwm.setPWM(point_id[which], 0, 0); 
+      pwm.setPWM(point_id[which], 0, 0);              // turn off servo
       }
     else
       {
-        p_direction[which] = p_straight[which];
-        //pwm.setPWM(0, 0, direzioneP0);
+      p_direction[which] = p_straight[which];
       for (uint16_t pulselen = p_dev[which]; pulselen > p_straight[which]; pulselen--) {
-      pwm.setPWM(point_id[which], 0, pulselen);
-      delay(6);
-      }  
-      delay(500);
-        pwm.setPWM(point_id[which], 0, 0); 
+        pwm.setPWM(point_id[which], 0, pulselen);
+        delay(5);
       }
-      Serial.println(p_direction[which]);
-    time = millis();    
+      pwm.setPWM(point_id[which], 0, 0);              // turn off servo
+      }
 }
-//Serial.println(direzioneP0);
-
-//pwm.setPWM(0, 0, 0);  // turn off PWM signal to channel 0
-//myservo.write(direzione,7,false); 
-//if (myservo.read() > 90)      //opzionale: led indicazione di stato dello scambio
-//   digitalWrite(13, HIGH);
-//else
-//   digitalWrite(13, LOW);
-
-// previous = reading;
-//}
 
