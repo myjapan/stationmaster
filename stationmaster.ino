3de4b7a66ebabdb2b8f71fdf6223435cdb1f7bd4
myjapan <myjapan@users.noreply.github.com>
2016-09-12 20:23:37 +0200

    Light the right led during setup() + minor changes

--- stationmaster.ino
+++ stationmaster.ino
@@ -12,17 +12,17 @@ Adafruit_PWMServoDriver led1d = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver led2d = Adafruit_PWMServoDriver(0x41);
 
 Adafruit_PWMServoDriver* bp[] = {&pwm2, &pwm2, &pwm2, &pwm2, &pwm2, &pwm2, &pwm2, &pwm2, &pwm2};
-Adafruit_PWMServoDriver* bls[] = {&led1s, &led2s, &led1s, &led1s, &led1s, &led1s, &led1s, &led1s, &led1s};
-Adafruit_PWMServoDriver* bld[] = {&led2d, &led1d, &led1d, &led1d, &led1d, &led1d, &led1d, &led1d, &led1d};
+Adafruit_PWMServoDriver* bls[] = {&led2d, &led1d, &led1s, &led1s, &led1s, &led1s, &led1s, &led1s, &led1s};
+Adafruit_PWMServoDriver* bld[] = {&led1s, &led2s, &led1d, &led1d, &led1d, &led1d, &led1d, &led1d, &led1d};
 
 byte button_total = 9;
 int button_id[] = {13,8,12,11,6,5,10,7,9};            // toggle buttons
-int point_id[] = {15,14,13,12,11,10,9,8,7};             // points connected to Adafruit 16-channel PWM servo driver
-int p_direction[] = {150,150,150,150,150,150,150,150,150};      // actual position
-int p_straight[] = {150,150,150,150,150,150,150,150,150};       // value to move the point in straight position
-int p_diverging[] = {270,270,270,270,270,270,270,270,270};      // value to move the point in diverging position
-int l_straight[] = {3,1,8,1,0,5,13,14,15};             // leds, straight position
-int l_diverging[] = {3,4,2,7,6,12,9,10,11};            // leds, diverging postition
+int point_id[] = {15,11,13,12,6,10,9,8,7};             // points connected to Adafruit 16-channel PWM servo driver
+int p_direction[] = {340,340,220,220,150,150,340,220,150};      // actual position
+int p_straight[] = {220,220,220,220,150,150,220,210,150};       // value to move the point in straight position
+int p_diverging[] = {340,340,340,340,270,270,340,330,270};      // value to move the point in diverging position
+int l_straight[] = {3,4,2,1,0,5,9,14,15};             // leds, straight position
+int l_diverging[] = {3,1,8,7,6,12,13,10,11};            // leds, diverging postition
 
 int reading;            // current reading from the input pin
 
@@ -59,12 +59,21 @@ void setup()
     pinMode(l_straight[i], OUTPUT);
     pinMode(button_id[i], INPUT);
     digitalWrite(button_id[i],HIGH);
-    bp[i]->setPWM(point_id[i], 0, p_straight[i]);
+    bp[i]->setPWM(point_id[i], 0, p_direction[i]);
     delay(200);
     bp[i]->setPWM(point_id[i], 0, 0);              // turn off servo
-    bld[i]->setPWM(l_diverging[i], 0, 0);
-    bls[i]->setPWM(l_straight[i], 4096, 0);
+    if ( p_direction[i]== p_straight[i])
+      {
+        bld[i]->setPWM(l_diverging[i], 0, 0);
+        bls[i]->setPWM(l_straight[i], 4096, 0);
+      }
+      else
+      {
+        bld[i]->setPWM(l_diverging[i], 4096, 0);
+        bls[i]->setPWM(l_straight[i], 0, 0);
+      }
   }
+  
   tone(3,1400,100);
   delay(100);
   noTone(3);
@@ -100,7 +109,7 @@ void toggle_point(byte which)
         bp[which]->setPWM(point_id[which], 0, pulselen);
 //                  Serial.print("Pulselen = ");
 //                  Serial.println(pulselen);
-        delay(5);
+        delay(7);
                   if (pulselen > (p_diverging[which]-(p_diverging[which]*.35)))
                   {
                     bld[which]->setPWM(l_diverging[which], 4096, 0);
